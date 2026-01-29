95827f8bac697d41aeff7d9d52efd6ae5ca5fb26
Ralphmens <87376272+Ralphmens@users.noreply.github.com>
2026-01-29 10:17:23 +0000

    Lab 1

---
+++ Lab2/environment.py
@@ -0,0 +1,4 @@
+import random as rnd
+
+def get_disaster_status():
+    return rnd.choice(["LOW", "MEDIUM", "HIGH"])
---
+++ Lab2/sensor_agent.py
@@ -0,0 +1,31 @@
+import spade
+from spade.agent import Agent
+from spade.behaviour import CyclicBehaviour
+import asyncio
+from environment import get_disaster_status
+
+
+class SensorAgent(Agent):
+    class MonitorBehaviour(CyclicBehaviour):
+        async def run(self):
+            severity = get_disaster_status()
+            print(f"[SensorAgent] Disaster severity: {severity}")
+
+            with open("event_log.txt", "a") as f:
+                f.write(f"Detected severity: {severity}\n")
+
+            await asyncio.sleep(5)
+
+    async def setup(self):
+        print("SensorAgent started...")
+        self.add_behaviour(self.MonitorBehaviour())
+
+
+async def main():
+    agent = SensorAgent("sensor@localhost", "password123")
+    await agent.start()
+    await asyncio.sleep(30)
+    await agent.stop()
+
+if __name__ == "__main__":
+    spade.run(main())
